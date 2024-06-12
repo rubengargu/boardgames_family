d3c4d3c147594a23c8543f358a88ab3ad421ab4d
Ruben Garcia <64198290+rubengargu@users.noreply.github.com>
2024-06-12 09:21:51 +0000

    se empiezan a cambiar cosas de cara a crear el proyecto barna batch

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import  "../../styles/navbar.css"
+import logo from "../../img/logo.png"
 
 export const Navbar = () => {
 	return (
@@ -13,16 +14,17 @@ export const Navbar = () => {
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
         <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a href="/" className="li-logo"><img src={logo} className="navbar-brand logo col-sm-4 " ></img></a>
+        <a className="color-link" href="#">¿Quienes somos?</a>
+        <a className="color-link" href="#" >¿Donde hemos estado?</a>
+        <a className="color-link" href="#" >¿Que estamos jugando actualmente?</a>
+       
         
-        <Link to={"/formulario"} className="favoritos-button">Formulario </Link>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Hello</a>
       </div>
-	  <a className="user"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person" viewBox="0 0 16 16">
-  <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
-</svg>Registrate!</a>
+	
     </div>
   </div>
 </nav>
 	);
 };
+
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,17 +8,10 @@ export const Home = () => {
 
 	return (
 		<div className="inicio">
-		<h1>Bienvenid@s a Boardgames4All</h1>
-		<p>El juego es algo innato en el ser humano. Desde que nacemos la manera en la que nos aprendemos a relacionar
-			es a través de él. Y cuando mejor aprendemos, es de esta manera.</p>
-		<p>Y hasta hace muy poco,parecía que, al crecer, dejábamos de hacerlo, trabajando y buscando hobbys
-			que nos hicieran "crecer".</p>
-			<h5>Por suerte esto cambió</h5>	
-			<p>Y es que desde hace unos años y sobretodo con la pandemia, el mundo de los juegos de mesa ha 
-			experimentado un crecimiento brutal por parte de gente de todas las edades, siendo la pregunta más
-			clásica</p>
-			<h3>¿Qué juego me recomiendas</h3>
-			<p>Pues quédate aquí y lo sabrás</p>
+		
+	<h1>Somos la Barna Batch!</h1>
+	<p>Somos un grupo de jugadores de Barcelona que gracias a eventos varios nos hemos ido uniendo con un objetivo</p>
+	<h2>Disfrutar de este juego!</h2>
 
 
 		</div>
